1e9a9318b70be07b872c0903feaec181e517e40a
natnael340 <natnaeltilahun157@gmail.com>
2023-02-03 09:12:17 +0300

    Splash Screen Completed

---
+++ src/components/molecules/title.tsx
@@ -0,0 +1,8 @@
+import {View, Text} from 'react-native';
+import React from 'react';
+
+const title = () => {
+  return <View className="flex-row"></View>;
+};
+
+export default title;
---
+++ src/components/pages/OnBoardOneScreen.tsx
@@ -0,0 +1,13 @@
+import {View} from 'react-native';
+import React from 'react';
+import Title from '../molecules/title';
+
+const OnBoardOneScreen = () => {
+  return (
+    <View>
+      <Title />
+    </View>
+  );
+};
+
+export default OnBoardOneScreen;
